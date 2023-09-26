4ba5110d0d805b83a557f9717b95dedb602005cf
Shailendra Jain <shailendra.jain0894@gmail.com>
2023-09-27 03:55:20 +0530

    Basic Server making file in node

---
+++ basicServer.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+//------------------ First Simple server type 
+
+// http.createServer((req, res)=>{
+//     res.write("<h1>Hello Shailendra Jain</h1>");
+//     res.end();
+// }).listen(3000);
+
+//------------------ Second Simple server type
+
+const dataControl = (req, res) => {
+    res.write("<h1>Hello, This is Shailendra Jain</h1><br/><h2>Software Engineer</h2>");
+    res.end();
+}
+
+http.createServer(dataControl).listen(3000)
