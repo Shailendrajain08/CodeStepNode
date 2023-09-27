cd1eaa4ed640f3632cd1b6e10587726d8d01246d
Shailendra Jain <shailendra.jain0894@gmail.com>
2023-09-28 04:18:37 +0530

    Working with .gitignore and chalk library

---
+++ chalk.js
@@ -0,0 +1,8 @@
+// import chalk from 'chalk';
+const chalk = require('chalk');
+
+// Using chalk library we need chalk version of 2.3.0, you can donwload that version by using 'npm i chalk@2.3.0' 
+
+console.log(chalk.blue('Hello world!'));
+console.log(chalk.red('Hello world!'));
+console.log(chalk.yellow('Hello Shailendra!'));
