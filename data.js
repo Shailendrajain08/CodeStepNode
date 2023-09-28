4ddbe402f67891ceed64b5be1d79812189aed513
Shailendra Jain <shailendra.jain0894@gmail.com>
2023-09-29 03:40:54 +0530

    Creating Basic API

---
+++ data.js
@@ -0,0 +1,9 @@
+// Sending data to basicApi
+
+const data = [
+    {name:'Shailendra', email: 'shailendra@test.com', contact:'99990'},
+    {name:'Dharmendra', email: 'dharmendra@test.com', contact:'88880'},
+    {name:'Lokendra', email: 'lokendra@test.com', contact:'77770'},
+    {name:'Shailu', email: 'shailu@test.com', contact:'66660'}
+]
+module.exports = data;
