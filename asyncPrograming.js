ea0ed62db52a0c3568cbdefd99a20db27938ff7b
Shailendra Jain <shailendra.jain0894@gmail.com>
2023-10-08 02:19:33 +0530

    Async Programming Handling

--- asyncPrograming.js
+++ asyncPrograming.js
@@ -1,3 +1,5 @@
+// ----------------------- Async Programming Example
+
 // console.log("var execute");
 
 // setTimeout(()=>{
@@ -8,13 +10,34 @@
 
 // async programming do not wait to for block of code, function or any code to run completly if its taking time to execute it will run new set of code immidietly, previous code is taking time to execute that makes our application fast and superiour
 
+// -------------------------- Async Programming DrawBack Example
+
+// let a = 10; 
+// let b = 0;
+
+// setTimeout(()=>{
+//     b = 30;
+// }, 3000)
+
+// console.log(a+b);
+
+// drawback of async programming is that if some necessary code is need to execute before it's next set of code and that code is taking time then it will not wait for that code it will immediatly execute next set of code and user will not get desired output.
+
+// -------------------------- Async Programming Handling Example
+
 let a = 10;
 let b = 0;
 
-setTimeout(()=>{
-    b = 30;
-}, 3000)
+let waitingData = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        resolve(50);
+    }, 3000)
+})
 
-console.log(a+b);
+waitingData.then((data)=>{
+    console.log(a+data);
 
-// drawback of async programming is that if some necessary code is need to execute before it's next set of code and that code is taking time then it will not wait for that code it will immediatly execute next set of code and user will not get desired output.
+})
+
+
+// we can handle async drwaback or async programing using promise. Promise will wait for internal data for asnyc data to execute and it will run that data immediatly once that data will resolve.
