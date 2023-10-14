97028a956118727a138b024f721c27df88cdabe1
Shailendrajain08 <shailendra.jain0894@gmail.com>
2023-10-15 01:35:32 +0530

    Formating the code only

--- connectingMongoDBWithPromise.js
+++ connectingMongoDBWithPromise.js
@@ -3,7 +3,7 @@ const url = 'mongodb+srv://shailendrajain0894:0000@cluster0.dtmkl8p.mongodb.net/
 const client = new MongoClient(url);
 const dataBase = 'e-comm'
 
-async function dbConnect(){
+async function dbConnect() {
 
     let result = await client.connect()
     let db = result.db(dataBase)
@@ -15,28 +15,28 @@ async function dbConnect(){
 }
 
 // ++++++++++++++++++++ First way to handle Promise +++++++++++++++++++++
-// // We are getting promise {<pending>} , for handling we have to use then to handle promise 
+// We are getting promise {<pending>} , for handling we have to use then to handle promise 
 
-// dbConnect().then((res)=>{
-// // for full collection data ======================
-// console.log(res.find({}).toArray().then((data)=>{
-//     console.log(data);
-// }));
+dbConnect().then((res) => {
+    // for full collection data ======================
+    console.log(res.find({}).toArray().then((data) => {
+        console.log(data);
+    }));
 
-// //for single data ======================
-// // console.log(res.find({name:"S23 ultra"}).toArray().then((data)=>{
-// //     console.log(data);
-// // }));
-// })
+    //for single data ======================
+    // console.log(res.find({name:"S23 ultra"}).toArray().then((data)=>{
+    //     console.log(data);
+    // }));
+})
 // ++++++++++++++++++++++++++++ End ++++++++++++++++++++++++++++++++
 
 
 // ++++++++++++++++++++ Second way to handle Promise +++++++++++++++++++++
 
-const main = async () =>{
-let data = await dbConnect();
-data = await data.find().toArray();
-console.log(data)
+const main = async () => {
+    let data = await dbConnect();
+    data = await data.find().toArray();
+    console.log(data)
 }
 
 main()
