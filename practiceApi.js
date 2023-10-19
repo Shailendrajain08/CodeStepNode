5ec539d2a80461226265a7b4c89a6fbf327331e7
Shailendrajain08 <shailendra.jain0894@gmail.com>
2023-10-20 01:57:41 +0530

    creating post api method and connecting to mongodb using express

--- practiceApi.js
+++ practiceApi.js
@@ -2,6 +2,9 @@ const express = require('express');
 const dbConnect = require('./MongoDB');
 const app = express();
 
+//For taking data from postman
+app.use(express.json());
+
 // get api method using express
 app.get('/', async (req, res) => {
     let data = await dbConnect();
@@ -9,6 +12,10 @@ app.get('/', async (req, res) => {
     res.send(result)
 })
 
-
+app.post('/', async (req, res) => {
+    let data = await dbConnect();
+    let result = data.insertOne(req.body);
+    res.send(result)
+}) 
 
 app.listen(3000)
